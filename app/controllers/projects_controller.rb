44f14854c676809af20b7bb18f078fa218ce10cc
Nagachandra <nagachandra.kn89@gmail.com>
2014-05-05 23:00:15 +0530

    Redirect the users back to the projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,8 @@
 class ProjectsController < ApplicationController
 
+before_action :set_project, only:[:show,:edit,:update,:destroy] 
+
+
 	def index
 		@projects = Project.all
 	end
@@ -55,5 +58,13 @@ class ProjectsController < ApplicationController
 	def project_params
 		params.require(:project).permit(:name,:description)
 	end
+	
+	def set_project
+		@project = Project.find(params[:id])
+		rescue ActiveRecord::RecordNotFound
+		flash[:alert] = "The project you were looking for is not found."
+		redirect_to projects_path
+	end
+	
 
 end
--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -2,4 +2,12 @@ require 'spec_helper'
 
 describe ProjectsController do
 
+it "displays an error message for missing project to" do
+
+get :show ,id: "not-here"
+expect(response).to redirect_to(projects_path)
+message = "The project you were looking for is not found."
+expect(flash[:alert]).to eql(message)
+
+end
 end
