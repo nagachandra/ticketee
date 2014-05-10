eab97730ff358872dd9d5a0dce666a5dd1740553
Nagachandra <nagachandra.kn89@gmail.com>
2014-05-10 14:53:53 +0530

    Implemented sign in

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Ticketee::Application.routes.draw do
   
   root "projects#index"
   
+  post "/signin" , to: "sessions#create"
+  
+  get "/signin",to: "sessions#new"
+  
 resources :users
   
 resources :projects do
