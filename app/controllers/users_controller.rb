6e7a7f4be186ff1579d9bc53f364d5c837cc3253
Nagachandra <nagachandra.kn89@gmail.com>
2014-05-10 13:21:54 +0530

    Implement profile pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,20 @@ class UsersController < ApplicationController
   def new
 	@user = User.new
   end
+  
+  def edit
+	@user = User.find(params[:id])
+  end
+  
+  def update
+	@user = User.find(params[:id])
+	if @user.update(user_params)
+		flash[:notice] = "Profile has been updated."
+		redirect_to @user
+	else
+	render action: "edit"
+	end
+  end
 
   def create
 	@user = User.new(user_params)
@@ -17,6 +31,7 @@ class UsersController < ApplicationController
   end
 
   def show
+	@user = User.find(params[:id])
   end
   
   private
